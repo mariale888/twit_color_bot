b7d1d8d02f2c8f2d47d063281477bd6a88dc4b2b
Maria Alejandra Montenegro <mariale@mariales-mbp.home>
2015-03-23 10:23:48 -0400

    update bot

--- my_bot/rtd2.js
+++ my_bot/rtd2.js
@@ -21,7 +21,7 @@ my_cities.push({
                 key:['Sydney'],
                 value: [-33.8683, 151.2086]});
 my_cities.push({
-                key:['Egypt'],
+                key:['Cairo'],
                 value: [26.0000, 30.0000]});
 my_cities.push({
                 key:['Paris'],
@@ -53,9 +53,9 @@ my_cities.push({
 my_cities.push({
                 key:['Tehran'],
                 value: [35.6961, 51.4231]});
-
-
-
+my_cities.push({
+                key:['Mumbai'],
+                value: [18.9750, 72.8258]});
 
 //get date string for today's date (e.g. '2011-01-01')
 function datestring () {
@@ -74,7 +74,7 @@ function sendTweet(my_color) {
       if(err) return handleError(err);
         var mediaIdStr = data.media_id_string;
       var params = { status: my_color, media_ids: mediaIdStr } 
-        bot.tweet(params, function (err1, data1,reply1) {
+        bot.tweet_with_art(params, function (err1, data1,reply1) {
           if(err1) return handleError(err1);
           //console.log('\nTweet: ' + (reply1 ? reply1.text : reply1));
       })
@@ -91,89 +91,44 @@ setInterval(function() {
   // get the city of the hour
   var rand_ = Math.floor(Math.random() * my_cities.length);
   var my_city = my_cities[rand_].key[0]
-  var my_color = 'The current color temperature of '+ my_city + '! #colorweather ';
+  var my_color = '';
 
   var rand = Math.random();
   var chance = new Chance();
  
   // choose a random city
  if(rand <= 0.50) {      
-   // Geocoding
-      var city   = chance.coordinates();
-      city_name  = chance.city(); 
-      city       = city.split(",");
-
-      bot.random_city(city,city_name,function(returnValue) {
-        // use the return value here instead of like a regular (non-evented) return value
-          if(returnValue != null) {
-            //we got city
-            if(returnValue.length > 1)  {
-               bot.getForecast(city,function(returnV)  {
-
-                my_color = 'The current color temperature of '+ returnValue + '!';//' #colorweather ';  
-                bot.makeSentence(returnValue,function(callback)
-                {
-                  my_color = callback + '! #colorweather ';
-                  sendTweet(my_color);
-                });
-              });
-            }
-            else {
-                bot.getForecast([returnValue.lat , returnValue.lng], function(returnV)  {
-                   my_color = 'The current color temperature of '+ city_name + '! #colorweather ';  
-                    bot.makeSentence(city_name,function(callback)
-                    {
-                      my_color = callback + '! #colorweather ';
-                      endTweet(my_color);
-                    });
-               });
-            }
-          }
-          else  {
-            bot.getForecast(my_cities[rand_].value, function(returnV)  {
-               bot.makeSentence(my_city,function(callback)
-                {
-                  my_color = callback + '! #colorweather ';
-                  sendTweet(my_color);
-                });
-            });
-          }
-      });
+      my_city  = chance.city();
   } 
-  //  add random hashtag
-  else {    
-
-    // Instantiate Chance so it can be used
-    var my_random_hashtag = chance.hashtag();
-    my_color = my_color + my_random_hashtag;
-    bot.getForecast(my_cities[rand_].value,function(returnV)  {
-      bot.makeSentence(my_city,function(callback)
-        {
-            my_color = callback + '! #colorweather ' + my_random_hashtag;
-            sendTweet(my_color);
-        });
-    }); 
-  }
-
-
-/*
- // post tweet with image
-  var media = 'out.png';
-  console.log(my_color);
-  bot.tweetMedia(media, function (err,data, reply) {
+  //make a friend    
+  if(rand <= 0.3) {
+    bot.mingle(function(err, reply) {
       if(err) return handleError(err);
-        var mediaIdStr = data.media_id_string;
-        //console.log(mediaIdStr);
-        //console.log(data);
-        var params = { status: my_color, media_ids: mediaIdStr } 
-        bot.tweet(params, function (err1, data1,reply1) {
-          if(err1) return handleError(err1);
-          //console.log('\nTweet: ' + (reply1 ? reply1.text : reply1));
-        })
-  })*/
+      var name = reply.screen_name;
+      console.log('\nNietzsche: followed @' + name);
+    });
+  }
+  
+
+  //get info and publish tweet
+  var info = '';
+  bot.getNews(my_city,function(returnValue)  { 
+
+      if(info == '')  {
+          info = returnValue;
+          bot.makeSentence_1(info,function(callback) {
+            my_color = callback + '! #' + my_city.replace(' ','');
+            
+            bot.tweet(my_color, function (err, reply) {
+               if(err) return handleError(err);
+               console.log('\nTweet: ' + (reply ? reply.text : reply));
+              })
+          });
+        }
+  });
 
 
-}, 10000); // change to hr1800000
+}, 1200000); // 30min = 1800000 // 20min = 1200000
 
 function handleError(err) {
   console.error('response status:', err.statusCode);
